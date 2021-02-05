49a53745633fdc35ca56decc3c5194dfce997d6f
Faschal <faschal21@gmail.com>
2021-02-05 13:23:25 +0700

    Added add transaction

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -3,12 +3,7 @@ import AppReducer from './AppReducer';
 
 // Initial state
 const initialState = {
-  transaction: [
-    { id: 1, text: 'Flower', amount: -20 },
-    { id: 2, text: 'Salary', amount: 300 },
-    { id: 3, text: 'Book', amount: -10 },
-    { id: 4, text: 'Camera', amount: 150 }
-  ]
+  transaction: []
 }
 
 //Create context
@@ -26,7 +21,14 @@ export const GlobalProvider = ({ children }) => {
     });
   }
 
-  return (<GlobalContext.Provider value={{ transaction: state.transaction, deleteTransaction }}>
+  function addTransaction(transaction) {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction
+    });
+  }
+
+  return (<GlobalContext.Provider value={{ transaction: state.transaction, deleteTransaction, addTransaction }}>
     {children}
   </GlobalContext.Provider>)
 }
